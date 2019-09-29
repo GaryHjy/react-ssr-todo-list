bb8cedbf542d41e48a419907464c485bf571ae64
GaryHjy <782316959@qq.com>
2019-09-29 15:32:33 +0800

    feat: 添加egg-validate-plus

--- config/config.default.js
+++ config/config.default.js
@@ -5,5 +5,18 @@ module.exports = {
   static: {
     prefix: '/',
     dir: resolvePath('../dist')
+  },
+  validatePlus: {
+    resolveError(ctx, errors) {
+      if (errors.length) {
+        ctx.type = 'json';
+        ctx.status = 400;
+        ctx.body = {
+          code: 400,
+          error: errors,
+          message: '参数错误',
+        };
+      }
+    }
   }
 }
--- config/plugin.js
+++ config/plugin.js
@@ -1,3 +1,6 @@
 module.exports = {
-
+  validatePlus: {
+    enable: true,
+    package: 'egg-validate-plus'
+  }
 }
