8ec90d9078275a110c13816c54305a85cbbaf1b2
GaryHjy <782316959@qq.com>
2019-10-09 11:30:49 +0800

    feat: 登录页面添加找回密码链接

--- web/page/login/index.js
+++ web/page/login/index.js
@@ -3,6 +3,9 @@ import { Form, Icon,  Input, Button } from 'antd';
 
 import './index.less'
 import LoginLayout from '@/layout/login/index';
+import LoginCard from '@/components/LoginCard/index';
+import { letterNumberDashUnderlinePattern } from '@/utils/pattern';
+import { Link } from 'react-router-dom';
 
 const withIcon = (type) => {
   return <Icon type={type} style={{ color: 'rgba(0,0,0,.25)' }} />
@@ -25,14 +28,38 @@ class LoginForm extends Component {
     })
   }
 
+  validateToUserName(rule, value, callback) {
+    if (value && value.length < 6) {
+      callback('用户名长度不能小于6');
+    }
+    if (value && !letterNumberDashUnderlinePattern.test(value)) {
+      callback('用户名只能包含数字、字母、中划线、下划线');
+    }
+    callback();
+  }
+
+  validateToNextPassword(rule, value, callback) {
+    const { form } = this.props;
+    if (value && value.length < 6) {
+      callback('密码长度不能小于6');
+    }
+    if (value && this.state.confirmDirty) {
+      form.validateFields(['confirm'], { force: true });
+    }
+    callback();
+  };
+
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
       <Form onSubmit={(e) => this.handleSubmit(e)} className="login-form">
-        <Form.Item>
+        <Form.Item hasFeedback>
           {getFieldDecorator('username', {
             rules: [
-              { required: true, message: '请输入用户名', whitespace: true }
+              { required: true, message: '请输入用户名', whitespace: true },
+              {
+                validator: (...arg) => this.validateToUserName(...arg),
+              }
             ],
           })(
           <Input
@@ -40,10 +67,13 @@ class LoginForm extends Component {
             placeholder="用户名" />,
           )}
         </Form.Item>
-        <Form.Item>
+        <Form.Item hasFeedback>
           {getFieldDecorator('password', {
             rules: [
-              { required: true, message: '请输入密码', whitespace: true }
+              { required: true, message: '请输入密码', whitespace: true },
+              {
+                validator: (...arg) => this.validateToNextPassword(...arg),
+              },
             ],
           })(
           <Input
@@ -52,6 +82,9 @@ class LoginForm extends Component {
             placeholder="密码" />,
           )}
         </Form.Item>
+        <div className='login-form__links'>
+          <Link to='/retrieve-password'>忘记密码</Link>
+        </div>
         <Form.Item className="login-form__buttons">
           <Button type="primary" htmlType="submit">
             登录
@@ -71,8 +104,9 @@ class Login extends Component {
   render() {
     return (
       <div className='login-container center'>
-        <h3 className='login-container_title'>用户登录</h3>
-        <LoginForm {...this.props}/>
+        <LoginCard title="用户登录">
+          <LoginForm {...this.props} />
+        </LoginCard>
       </div>
     )
   }
